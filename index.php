a291d99caba8258cef74efa537a1b0b0f3e55c2a
zhongkouwei <726442240@qq.com>
2017-05-16 14:57:47 +0800

    代码初始化

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+// +----------------------------------------------------------------------
+// | ThinkPHP [ WE CAN DO IT JUST THINK ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2006-2014 http://thinkphp.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: liu21st <liu21st@gmail.com>
+// +----------------------------------------------------------------------
+
+// 应用入口文件
+
+// 检测PHP环境
+if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
+
+define('APP_DEBUG',true);
+
+define('APP_PATH','./App/');
+
+require './ThinkPHP/ThinkPHP.php';
